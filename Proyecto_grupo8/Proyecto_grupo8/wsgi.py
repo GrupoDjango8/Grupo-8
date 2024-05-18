fec28468a3c225b8d22817bf20a7cd2f2289a262
FedynaCarlos <jfedyna@outlook.com>
2024-05-18 18:27:21 -0300

    instale el entorno virtual y tambien en otra carpeta est instalado Django

---
+++ Proyecto_grupo8/Proyecto_grupo8/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Proyecto_grupo8 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto_grupo8.settings')
+
+application = get_wsgi_application()
