5de01fa483299c6298a3736296a452b26e5fe40e
Giana <113945161+giana2022@users.noreply.github.com>
2024-05-18 21:12:55 -0300

    agregué entorno virtual (o eso creo)

---
+++ grupo8Trabajo/grupo8Trabajo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for grupo8Trabajo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grupo8Trabajo.settings')
+
+application = get_wsgi_application()
