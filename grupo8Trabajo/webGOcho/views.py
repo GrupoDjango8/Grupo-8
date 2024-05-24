e122f168b13ee781155b3f81e6c39036098ba63e
Giana <113945161+giana2022@users.noreply.github.com>
2024-05-24 14:28:12 -0300

    Agregué el url del registro

--- grupo8Trabajo/webGOcho/views.py
+++ grupo8Trabajo/webGOcho/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+#Renderizado del index
 def index(request):
     return render(request, "webGOcho/index.html")
 
-
-def pedidos(request):
-    pass
+#Renderizado del registro
+def registro(request):
+    return render(request, "webGOcho/registro.html")
