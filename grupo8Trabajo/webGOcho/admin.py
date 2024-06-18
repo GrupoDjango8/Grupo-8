a41591860f6f8c1157ea4d5d6caf2e0181a65c51
Giana <113945161+giana2022@users.noreply.github.com>
2024-06-17 23:41:11 -0300

    Cree superuser de admin y cree los modelos

--- grupo8Trabajo/webGOcho/admin.py
+++ grupo8Trabajo/webGOcho/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cliente, Menu, Pedido, PedidoItem
+
+admin.site.register(Cliente)
+admin.site.register(Menu)
+admin.site.register(Pedido)
+admin.site.register(PedidoItem)
--- grupo8Trabajo/webGOcho/views.py
+++ grupo8Trabajo/webGOcho/views.py
@@ -9,7 +9,8 @@ def index(request):
     # Hay que harcodear esto
     menu = Menu.objects.all()
     contexto = {
-        'objetos_menu': menu
+        'objetos_menu': menu,
+        'mesas':[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     }
     #print(contexto['objetos_menu'])
     return render(request, "webGOcho/index.html", contexto)
