c7b026041ca98a8623e5fd314c730bb0b4a8e8a9
Giana <113945161+giana2022@users.noreply.github.com>
2024-07-03 00:56:18 -0300

    corregí algo que no funcionaba

--- grupo8Trabajo/webGOcho/static/webGOcho/js/administracion.js
+++
@@ -1,9 +0,0 @@
-let btnEliminar = document.querySelectorAll('.btn-danger');
-/*console.log(btnEliminar)*/
-
-btnEliminar.forEach(btn => {
-    btn.addEventListener('click', function (e) {
-        e.preventDefault()
-        alert('Usted está eliminando este elemento')
-    })
-})
